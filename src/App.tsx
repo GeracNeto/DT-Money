f2c8f567d18e5886671780c3c95518299b1ae4b3
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-01 21:23:39 -0300

    update(App): add Summary component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 import { ThemeProvider } from "styled-components";
+import { Summary } from "./components/Summary";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Transactions />
+      <Summary />
 
       <GlobalStyle />
     </ThemeProvider>
