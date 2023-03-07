d19bc98194c3626b0e0bf4ee1ed41d00449fc378
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-07 20:20:13 -0300

    update(App, Transaction): put table on the Transaction page

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,11 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 import { ThemeProvider } from "styled-components";
-import { Summary } from "./components/Summary";
-
-import {
-  PriceHighLight,
-  TransactionsContainer,
-  TransactionsTable,
-} from "./pages/Transaction/styles";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Transactions />
-      <Summary />
-
-      <TransactionsContainer>
-        <TransactionsTable>
-          <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
-          </tbody>
-        </TransactionsTable>
-      </TransactionsContainer>
-
       <GlobalStyle />
     </ThemeProvider>
   );
