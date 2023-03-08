908ff14d2aad148753e0d4038f95c85670ac62a6
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2023-03-07 22:09:29 -0300

    update(Header):
    -remove Dialogs - (it's wihtin NewTransactionModal)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
 
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -16,13 +17,7 @@ export function Header() {
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal></NewTransactionModal>
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
